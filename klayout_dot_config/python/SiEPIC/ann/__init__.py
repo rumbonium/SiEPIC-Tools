436a75986bdbd1dc5da3507f6605574b52f30e5d
rumbonium <hygunth@gmail.com>
2019-05-22 12:24:04 -0600

    commented out blow up code

--- klayout_dot_config/python/SiEPIC/ann/__init__.py
+++ klayout_dot_config/python/SiEPIC/ann/__init__.py
@@ -1,5 +1,5 @@
 # Alec Hammond's ANN Python integration
 
-from . import loadNN, netlist, waveguideNN, getSparams, NetlistDiagram, circuit_simulation, simulation, model, monte_carlo_simulation
-
+from . import netlist, waveguideNN, getSparams, NetlistDiagram, circuit_simulation, simulation, model, monte_carlo_simulation
+# loadNN,
 print('ANN Python integration (CamachoLab)')
